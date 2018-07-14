47a54911337c45c014b93b1848ee7eb66c3ac89f
Jonne Kanerva <jonne.kanerva@siili.com>
2018-07-14 13:39:13 +0300

    Tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,15 @@
-const dummy = () => {
-  return 1
+const favoriteBlog = blogs => {
+  if(blogs.length === 0)
+    return {}
+
+  if(blogs.length === 1)
+    return blogs[0]
+
+  const sortedBlogs = blogs.sort( (a, b) => {
+    return a.likes < b.likes
+  })
+
+  return sortedBlogs[0]
 }
 
 const totalLikes = blogs => {
@@ -9,6 +19,6 @@ const totalLikes = blogs => {
 }
 
 module.exports = {
-  dummy,
+  favoriteBlog,
   totalLikes
 }
