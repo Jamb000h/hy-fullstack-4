2b02facb83b2259513e4cd74e8d73c6ccf767569
Jonne Kanerva <jonne.kanerva@siili.com>
2018-07-24 17:17:17 +0300

    tehtävä 4.12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,26 +1,54 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+const formatBlog = blog => {
+  return {
+    id: blog._id,
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes
+  }
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs)
+  response.json(blogs.map(formatBlog))
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if(blog) {
+      response.json(formatBlog(blog))
+    } else {
+      response.status(404).json({ error: 'Blog not found' })
+    }
+  } catch (e) {
+    response.status(400).json({ error: 'Malformatted id' })
+  }
 })
 
 blogsRouter.post('/', async (request, response) => {
-  if(request.body.title === undefined)
-    return response.status(400).json( { error: 'Title is required!' } )
+  try {
+    if(request.body.title === undefined)
+      return response.status(400).json({ error: 'Title is required!' })
 
-  if(request.body.url === undefined)
-    return response.status(400).json( { error: 'URL is required!' } )
+    if(request.body.url === undefined)
+      return response.status(400).json({ error: 'URL is required!' })
 
-  if(request.body.likes === undefined)
-    request.body.likes = 0
+    if(request.body.likes === undefined)
+      request.body.likes = 0
 
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  const savedBlog = await blog.save()
+    const savedBlog = await blog.save()
 
-  response.status(201).json(savedBlog)
+    response.status(201).json(formatBlog(savedBlog))
+  } catch (e) {
+    console.log(e)
+    response.status(500).json({ error: 'Something went wrong' })
+  }
 })
 
 module.exports = blogsRouter
