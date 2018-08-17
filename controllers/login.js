77b7a9ea87b2886caf4d60e3bfcdb7c71d348309
Jonne Kanerva <jonne.kanerva@siili.com>
2018-08-17 15:35:36 +0300

    Send user id on login to support id matching in frontend

--- controllers/login.js
+++ controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post('/', async (request, response) => {
 
     const token = jwt.sign(userForToken, process.env.SECRET)
 
-    response.status(200).send({ token, username: user.username, name: user.name })
+    response.status(200).send({ token, id: user._id, username: user.username, name: user.name })
   } catch (exception) {
     return response.status(500).json({ error: exception })
   }
